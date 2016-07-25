8ae472b2fc4f4dca36c4d37ff738c69ab1700ab5
JurelP <patocjurel@gmail.com>
2016-07-25 11:03:00 +0800

    Binds data to spinners

--- app/src/main/java/com/jurelmp/currencies/MainActivity.java
+++ app/src/main/java/com/jurelmp/currencies/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -42,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
         mCalcButton = (Button) findViewById(R.id.btn_calc);
         mForSpinner = (Spinner) findViewById(R.id.spn_for);
         mHomSpinner = (Spinner) findViewById(R.id.spn_hom);
+
+        // controller: mediates model and view
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                // context
+                this,
+                // view layout you see when the spinner is closed
+                R.layout.spinner_closed,
+                mCurrencies
+        );
+
+        // view: layout youe see when the spinner is open
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // assign adapters to spinner
+        mHomSpinner.setAdapter(arrayAdapter);
+        mForSpinner.setAdapter(arrayAdapter);
     }
 
     @Override
