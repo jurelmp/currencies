6c7f4e6734cb2dab9b213c9ab0839ee039579248
JurelP <patocjurel@gmail.com>
2016-07-25 10:41:26 +0800

    Gets references to views defined in layout

--- app/src/main/java/com/jurelmp/currencies/MainActivity.java
+++ app/src/main/java/com/jurelmp/currencies/MainActivity.java
@@ -4,15 +4,30 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // defines members that correspond to Views in our layout
+    private Button mCalcButton;
+    private TextView mConvertedTextView;
+    private EditText mAmountEditText;
+    private Spinner mForSpinner, mHomSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // assign references to our Views
+        mConvertedTextView = (TextView) findViewById(R.id.txt_converted);
+        mAmountEditText = (EditText) findViewById(R.id.edit_amount);
+        mCalcButton = (Button) findViewById(R.id.btn_calc);
+        mForSpinner = (Spinner) findViewById(R.id.spn_for);
+        mHomSpinner = (Spinner) findViewById(R.id.spn_hom);
     }
 
     @Override
