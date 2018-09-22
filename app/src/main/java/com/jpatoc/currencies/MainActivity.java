35c48ba0cc05284c8225fbb0b0c13ee75b295003
Jurel P <patocjurel@gmail.com>
2018-09-22 22:16:50 +0800

    Remove imports not in use.

--- app/src/main/java/com/jpatoc/currencies/MainActivity.java
+++ app/src/main/java/com/jpatoc/currencies/MainActivity.java
@@ -11,7 +11,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
